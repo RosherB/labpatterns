f76c51ce7d235c5914e381e9ed0c30f06b6c60f4
ehu <ehu@U105289>
2023-10-31 13:20:47 +0100

    a

---
+++ src/adapter2/Main.java
@@ -0,0 +1,18 @@
+package adapter2;
+
+import domain.Covid19Pacient;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Covid19Pacient pacient =new Covid19Pacient("a", 35);
+		pacient.addSymptomByName(null, null);
+		pacient.addSymptomByName(null, null);
+		pacient.addSymptomByName(null, null);
+		
+		
+		
+	}
+
+}
--- src/adapter2/PacientTableModel.java
+++ src/adapter2/PacientTableModel.java
@@ -1,35 +1,51 @@
 package adapter2;
 
+import java.util.Iterator;
+import java.util.Vector;
+
 import javax.swing.table.AbstractTableModel;
 
-import domain.Covid19Pacient;
+
+import domain.*;
 
 public class PacientTableModel extends AbstractTableModel {
 	  protected Covid19Pacient pacient;
 	  protected String[] columnNames =
 	    new String[] { "Name", "Symptom", "Weight" };
 
+	  private Vector<Symptom> sintomas = new Vector<Symptom>();
 	  public PacientTableModel(Covid19Pacient p) {
 	    this.pacient=p;
+	    Iterator<Symptom> i = pacient.getSymptoms().iterator();
+	    while (i.hasNext())
+	    {
+	    	sintomas.add(i.next());
+	    }
 	  }
 
 	  public int getColumnCount() {
 	    // Challenge!
-		 return 1;
+		 return 2;
 	  }
 
 	  public String getColumnName(int i) {
 	    // Challenge!
-		  return "Column name 1";
+		  return columnNames[i];
 	  }
 
 	  public int getRowCount() {
 	    // Challenge!
-		  return 1;
+		  
+		  return pacient.getSymptoms().size();
 	  }
 
 	  public Object getValueAt(int row, int col) {
 	    // Challenge!
-		  return "value";
+		  
+		  
+		 if(col==0) return sintomas.get(row).getName();
+		 if(col==1) return pacient.getWeight(sintomas.get(row));
+		 
+		 return null;
 	  }
 	}
